ff267ddb7271a2d9ecc3b0bc768863b700791ca0
Abdulramon Lasisi <lasisiolalekan081@gmail.com>
2023-01-03 14:42:06 +0100

    project-initialized

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Header } from './components'
+
+const App = () => {
+  return (
+    <div className='w-screen h-auto flex flex-col'>
+      <Header />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <div className='fixed w-screen z-50 bg-slate-300 p-6 px-16'>
+      <div className='hidden md:flex w-full h-full'></div>
+      <div className='flex md:hidden w-full h-full'></div>
+    </div>
+  )
+}
+
+export default Header
