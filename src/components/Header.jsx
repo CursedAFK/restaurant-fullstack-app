409ea8800d977777012c1773442d7943d3367d36
Abdulramon Lasisi <lasisiolalekan081@gmail.com>
2023-01-03 23:07:35 +0100

    header done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,45 @@
-const Header = () => {
+import { MdShoppingBasket } from 'react-icons/md'
+import Logo from '../img/logo.png'
+import Avatar from '../img/avatar.png'
+
+function Header() {
   return (
-    <div className='fixed w-screen z-50 bg-slate-300 p-6 px-16'>
-      <div className='hidden md:flex w-full h-full'></div>
+    <header className='fixed w-screen z-50 p-6 px-16'>
+      <div className='hidden md:flex w-full h-full items-center justify-between'>
+        <div className='flex items-center gap-2'>
+          <img src={Logo} alt='logo' className='w-10 object-contain' />
+          <p className='text-headingColor text-xl font-bold'>City</p>
+        </div>
+        <div className='flex items-center gap-8'>
+          <ul className='flex items-center gap-8'>
+            <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer'>
+              Home
+            </li>
+            <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer'>
+              Menu
+            </li>
+            <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer'>
+              About Us
+            </li>
+            <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer'>
+              Service
+            </li>
+          </ul>
+          <div className='relative flex items-center justify-center'>
+            <MdShoppingBasket className='text-textColor text-2xl cursor-pointer' />
+            <div className='w-5 h-5 rounded-full bg-cartNumBg flex items-center justify-center absolute -top-2 -right-2'>
+              <p className='text-xs text-white font-semibold'>2</p>
+            </div>
+          </div>
+          <img
+            src={Avatar}
+            alt='user-profile'
+            className='w-10 min-w-[40px] h-10 min-h-[40px] object-cover drop-shadow-2xl cursor-pointer'
+          />
+        </div>
+      </div>
       <div className='flex md:hidden w-full h-full'></div>
-    </div>
+    </header>
   )
 }
 
